e8a01baf6b60e7ee6942ab96c081542e158cc212
xiaoyuzhzh <xiaoyuzhzh@163.com>
2016-08-30 16:30:18 +0800

    模拟数据工具，增加List属性赋值，增加自定义实现类赋值

--- src/main/java/org/iceberg/utils/MockValueUtil.java
+++ src/main/java/org/iceberg/utils/MockValueUtil.java
@@ -1,15 +1,21 @@
-package org.iceberg.utils;
+package com.xkeshi.utils;
 
-import com.alibaba.fastjson.JSON;
-
+import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xiaoy on 2016/7/19.
  */
 public class MockValueUtil {
+
+    public static boolean debug = false;
 
     /**
      * 根据传入的type类型，创建实例并返回值
@@ -18,12 +24,18 @@ public class MockValueUtil {
      * @return
      */
     public static <T> T mockValue(Class<T> clazz){
+        if(clazz.isInterface()){
+            return null;
+        }
+
         try {
             T t = clazz.newInstance();
             setMockValue(t);
             return t;
         } catch (InstantiationException e) {
+            if(debug) e.printStackTrace();
         } catch (IllegalAccessException e) {
+            if(debug) e.printStackTrace();
         }
         return null;
     }
@@ -61,8 +73,9 @@ public class MockValueUtil {
         for(Field f : fields){
             f.setAccessible(true);
             try {
-                if (mockFieldValue(t, f)) continue;
+                mockFieldValue(t, f);
             } catch (Exception e) {
+                if(debug) e.printStackTrace();
             }
         }
     }
@@ -75,36 +88,38 @@ public class MockValueUtil {
      * @return
      * @throws IllegalAccessException
      */
-    private static <T> boolean mockFieldValue(T t, Field f) throws IllegalAccessException {
+    private static <T> void mockFieldValue(T t, Field f) throws IllegalAccessException {
+
         //基本类型不做处理
         if(f.getType().isPrimitive()){
-            return true;
+            return ;
         }
+
 
         /***************       基本包装类型处理开始          **************/
         if(f.getType().equals(String.class)){
             f.set(t,String.valueOf("1"));
-            return true;
+            return ;
         }
         if(f.getType().equals(Long.class)){
             f.set(t,Long.valueOf(1l));
-            return true;
+            return ;
         }
         if(f.getType().equals(Integer.class)){
             f.set(t,Integer.valueOf(1));
-            return true;
+            return ;
         }
         if(f.getType().equals(Boolean.class)){
             f.set(t,Boolean.valueOf(true));
-            return true;
+            return ;
         }
         if(f.getType().equals(BigDecimal.class)){
             f.set(t,BigDecimal.valueOf(11));
-            return true;
+            return ;
         }
         if(f.getType().equals(Date.class)){
             f.set(t,new Date());
-            return true;
+            return ;
         }
         /***************       基本包装类型处理结束          **************/
 
@@ -112,48 +127,70 @@ public class MockValueUtil {
 
 
         /***************       指定类型处理开始          **************/
+        if(f.getType().equals(List.class)){
+            Type type = f.getGenericType();
+            if(type instanceof ParameterizedType){
+                ParameterizedType pt = (ParameterizedType)type;
+                Type types[] = pt.getActualTypeArguments();
+                if(types.length == 0){
+                    f.set(t,new LinkedList<>());
+                }else{
+                    Type tp = types[0];
+                    List l = new LinkedList<>();
+                    Class c = (Class)tp;
+                    if(c.isInterface()){
+                        System.out.println(c.getName()+" is a interface,can't be instantiated");
+                        f.set(t,new LinkedList<>());
+                    }else{
+                        l.add(mockValue(c));
+                        f.set(t,l);
+                    }
+                }
+            }
+            return;
+        }
+
+        if(f.get(t)!=null){//给已有对象赋值
+            setMockValue(f.get(t));
+            return ;
+        }
 
         /***************       指定类型处理结束          **************/
 
 
 
         f.set(t,mockValue(f.getType()));
-        return false;
+        return ;
     }
 
     public static void main(String[] args) {
-        System.out.println(JSON.toJSONString(mockValue(Result.class),true));
+//        MockValueUtil.debug = true;
+//        System.out.println(JSON.toJSONString(mockValue(Result.class),true));
+
+    }
+
+    static class Result{
+        private List<Serializable> a;
+
+        private Map<Object,List> b;
+
+        public List<Serializable> getA() {
+            return a;
+        }
+
+        public void setA(List<Serializable> a) {
+            this.a = a;
+        }
+
+        public Map<Object, List> getB() {
+            return b;
+        }
+
+        public void setB(Map<Object, List> b) {
+            this.b = b;
+        }
     }
 
 }
 
 
-class Result{
-    private String code;
-    private String description;
-    private Object Result;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Object getResult() {
-        return Result;
-    }
-
-    public void setResult(Object result) {
-        Result = result;
-    }
-}
